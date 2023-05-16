830315cf7c0c21dfed14df02b14c3cfc4ed68721
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-16 22:41:12 +0530

    BMI form initial commit

---
+++ src/BmiForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BmiForm = () => {
+    return (
+        <>
+            <form id='bmi-form' method="post">
+                
+            </form>
+        </>
+    );
+};
+
+export default BmiForm;
