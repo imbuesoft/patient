a3bd18a9758d2b6f722c02ee9d005048f067f640
unknown <imbuesoftworld@gmail.com>
2023-05-16 19:51:54 +0530

    bmi formula value with form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState({ "height": 200 });
+  const [bmi, setBmi] = useState(0);
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+
+  const submit = () => {
+    //console.log(data);
+    const bmi = (data.weight) / ((data.height) * (data.height));
+    setBmi(bmi);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form>
+          <div className="mb-3 mt-3">
+            <label htmlFor="email" className="form-label">
+              Height
+            </label>
+            <input onChange={handleChange}
+              type="number"
+              className="form-control"
+              id="height"
+              placeholder="Enter height"
+              name="height"
+              value={data.height}
+            />
+          </div>
+          <div className="mb-3 mt-3">
+            <label htmlFor="email" className="form-label">
+              Weight
+            </label>
+            <input onChange={handleChange}
+              type="number"
+              className="form-control"
+              id="weight"
+              placeholder="Enter weight"
+              name="weight"
+            />
+          </div>
+          <div className="mb-3 mt-3">
+            <label htmlFor="email" className="form-label">
+              Age
+            </label>
+            <input onChange={handleChange}
+              type="number"
+              className="form-control"
+              id="age"
+              placeholder="Enter age"
+              name="age"
+            />
+          </div>
+          <div className="mb-3 mt-3">
+            <label htmlFor="email" className="form-label">Male : <input onChange={handleChange} type="radio" id="gender" value="male" name="gender" /></label>
+            <label htmlFor="email" className="form-label">Female : <input onChange={handleChange} type="radio" id="gender" value="female" name="gender" /></label>
+          </div>
+          <button type="button" onClick={submit} className="btn btn-primary">Submit</button>
+        </form>
+        <div className="mt-3">
+          <h1>BMI : {bmi}</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
