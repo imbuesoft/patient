442f234e0c11e13f3d247b15e74f9c2b3715f53c
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-17 13:05:01 +0530

    Handle BMI form submit

--- src/features/BmiCalculatorForm/components/SelectFromList.jsx
+++ src/features/BmiCalculatorForm/components/SelectFromList.jsx
@@ -1,10 +1,10 @@
 
 
-const SelectFromList = ({ name, options, title, defaultOption, required }) => {
+const SelectFromList = ({ name, options, title, defaultOption, required, onChange }) => {
     return (
         <>
             <label htmlFor={name} className="form-label mt-3">{title} {required && <span className="text-danger">*</span>}</label>
-            <select className="form-select" name={name} id={name}>
+            <select className="form-select" name={name} id={name} onChange={onChange}>
                 <option className="form-control">{defaultOption ? defaultOption : ""}</option>
                 {
                     options.map((x, i) => <option key={i} value={x}>{x}</option>)
--- src/features/BmiCalculatorForm/pages/BmiForm.jsx
+++ src/features/BmiCalculatorForm/pages/BmiForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Input from '../components/Input';
 import SelectFromList from '../components/SelectFromList';
 
@@ -6,26 +7,39 @@ const BmiForm = () => {
 
     const gender = ["Male", "Female", "Other"]
 
+    const [data, setData] = useState({ height: 0, weight: 0, age: 0, gender: "" });
+    const [bmi, setBmi] = useState(0);
+
+    const handleChange = (e) => {
+        setBmi(0)
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const bmi = (data.weight) / (data.height ** 2);
+        setBmi(bmi.toFixed(2));
+    }
+
     return (
         <>
-        <div className="d-grid h-100">
-            <div className="m-auto container border border-2 rounded-4">
-                <form id='bmi-form' method="post">
-                    <h1 className="h1 text-center mt-3">BMI Calculator</h1>
-                    <Input title="Enter your height" required={true} name="height" type="number" />
-                    <Input title="Enter your weight" required={true} name="weight" type="number" right="kg" />
-                    <Input title="Enter your age" name="age" type="number" />
-                    <SelectFromList name="gender" options={gender} title="Select your gender" />
-                    <button type="submit" className="btn btn-primary btn-lg mt-5 d-flex py-3 rounded-4" >
-                        <div className="spinner-border me-3 d-none" role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
-                        Calculate BMI
-                    </button>
-                    <div className="space " style={{height:"1em"}}></div>
-                </form>
+            <div className="d-grid h-100">
+                <div className="m-auto container border border-2 rounded-4 shadow-lg">
+                    <form id='bmi-form' method="post" onSubmit={handleSubmit}>
+                        <h1 className="h1 text-center mt-3 ">BMI Calculator</h1>
+                        <Input onChange={handleChange} title="Enter your height" required={true} name="height" />
+                        <Input onChange={handleChange} title="Enter your weight" required={true} name="weight" right="kg" />
+                        <Input onChange={handleChange} title="Enter your age" name="age" type="number" />
+                        <SelectFromList onChange={handleChange} name="gender" options={gender} title="Select your gender" />
+                        {bmi && <h3 className="h3 mt-4">BMI : {bmi}</h3> || ""}
+                        <button type="submit" className="btn btn-primary btn-lg mt-4 d-flex py-3 rounded-4" >
+                            Calculate BMI
+                        </button>
+                        <div className="space" style={{ height: "1em" }}></div>
+                    </form>
+                </div>
+                
             </div>
-        </div>
         </>
     );
 };
