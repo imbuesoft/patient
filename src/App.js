de7cf9ca819f07c2bbaf13636c742f69a4052299
Harshalkatakiya <katakiyaharshl001@gmail.com>
2023-05-17 22:31:39 +0530

    Ant Design UI added in BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,69 +1,51 @@
 import { useState } from "react";
+import { Card, Form, Input, Radio, Button, Typography } from 'antd';
+
+const { Title } = Typography;
 
 const App = () => {
-  const border = { border: "0.5px solid black" };
   const [data, setData] = useState({
     height: "", weight: "", age: "", gender: ""
   });
   const [bmi, setBmi] = useState(0);
+
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   }
   const submit = () => {
-    console.log(data);
     const bmi = (data.weight) / ((data.height) * (data.height));
     setBmi(bmi.toFixed(2));
   }
-  return (  
+
+  return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-8">
-          <div className="card mt-5 border-0">
-            <div className="card-body shadow-lg rounded">
-              <form>
-                <h1 className="text-center">BMI Calculator</h1>
-                <div className="mb-3">
-                  <label htmlFor="Height" className="form-label">Height :</label>
-                  <input type="number" className="form-control" id="Height" placeholder="meters" name="height" onChange={handleChange} value={data.height} style={border} />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="Weight" className="form-label">Weight :</label>
-                  <input type="number" className="form-control" id="Weight" placeholder="kilograms" name="weight" onChange={handleChange} value={data.weight} style={border} />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="Age" className="form-label">Age :</label>
-                  <input type="number" className="form-control" id="Age" placeholder="years" name="age" onChange={handleChange} value={data.age} style={border} />
-                </div>
-
-                <div className="row row-cols-auto">
-                  <label className="col">Gender : </label>
-                  <div className="form-check mb-3 ms-0 col">
-                    <input className="form-check-input" type="radio" name="gender" id="Male" onChange={handleChange} value="Male" style={border} />
-                    <label className="form-check-label" htmlFor="Male">
-                      Male
-                    </label>
-                  </div>
-                  <div className="form-check mb-3 col">
-                    <input className="form-check-input" type="radio" name="gender" id="Female" onChange={handleChange} value="Female" style={border} />
-                    <label className="form-check-label" htmlFor="Female">
-                      Female
-                    </label>
-                  </div>
-                </div>
-                <button type="button" onClick={submit} className="btn btn-primary w-100">Submit</button>
-              </form>
-              <div className="result mt-3 text-center">
-                {
-                  bmi !== 0 ? (<h2>Result : {bmi}</h2>) : null
-                }
-
-              </div>
-            </div>
-          </div>
+      <Card title={<Title level={2} style={{ textAlign: 'center' }}>BMI Calculator</Title>} bordered={false} style={{ width: 600, margin: "0 auto", marginTop: "15px", backgroundColor: "rgb(155, 219, 233)" }}>
+        <Form layout="vertical">
+          <Form.Item label="Height (meters)">
+            <Input type="number" name="height" onChange={handleChange} value={data.height} />
+          </Form.Item>
+          <Form.Item label="Weight (kilograms)">
+            <Input type="number" name="weight" onChange={handleChange} value={data.weight} />
+          </Form.Item>
+          <Form.Item label="Age (years)">
+            <Input type="number" name="age" onChange={handleChange} value={data.age} />
+          </Form.Item>
+          <Form.Item label="Gender">
+            <Radio.Group onChange={handleChange} name="gender">
+              <Radio value="Male">Male</Radio>
+              <Radio value="Female">Female</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" block onClick={submit}>Submit</Button>
+          </Form.Item>
+        </Form>
+        <div className="result mt-3 text-center">
+          {bmi !== 0 && <Title level={4}>Result : {bmi}</Title>}
         </div>
-      </div>
+      </Card>
     </div>
   );
 }
 
-export default App;
+export default App; 
