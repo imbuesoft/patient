bd5e7760786a4ed1f63231b7b89d4fa8a117d61d
Jay Sanghani <133778202+jaysanghani3@users.noreply.github.com>
2023-05-19 10:19:59 +0530

    Create tag components for HMS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Tabs } from 'antd';
+import TabsData from "./TabsData.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const onChange = (key) => {
+  console.log(key);
+};
 
+const App = () => <Tabs defaultActiveKey="1" items={TabsData} centered size="large" onChange={onChange} />;
 export default App;
