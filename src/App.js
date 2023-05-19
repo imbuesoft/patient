a7f0f05f4f9b404ffd0506b1b8935139cbc91cb7
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-19 12:04:32 +0530

    Added Search feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import BmiCalculator from './features/BmiCalculator/';
+import BmiCalculatorBootstrap from './features/BmiCalculatorBootstrap';
+import Search from './features/Search/';
+import fruitsAndVegetables from './api/tempData.json'
+import { Typography } from 'antd';
+
+const { Title } = Typography;
 
 function App() {
   return (
     <>
-      <BmiCalculator />
+      {/***** BMI calculator bootstrap *****/}
+      {/* <BmiCalculatorBootstrap /> */}
+      
+      {/***** BMI calculator ant-design *****/}
+      {/* <BmiCalculator /> */}
+
+      {/***** Search box *****/}
+      <div className="container " style={{ height: "100vh" }}>
+        <Title className='mt-4'>Search fruits and vegetables by name, color or category</Title>
+        <div className="d-flex justify-content-center mt-5 h-100">
+          <Search data={fruitsAndVegetables} />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/features/Search/index.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import SearchBox from './pages/SearchBox';
+
+const index = (props) => {
+
+    return (
+        <>
+            <SearchBox {...props} />
+        </>
+    );
+};
+
+export default index;
