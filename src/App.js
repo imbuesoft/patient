5f9d25c4410586bad7a62426dfa37e675925b16a
Deep_Pandya <deeppandya2002@gmail.com>
2023-05-19 18:29:01 +0530

    "third upload"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
-//import Alert from "./Alert";
+import Alert from "./Alert";
 //import Alert2 from "./Alert2";
 //import Log from "./Log";
-import Navbar from "./Navbar";
+
 
 function App() {
   return (
-    <Navbar/>
-    //<Alert/>
+    <Alert/>
     //<Alert2/>
     //<Log/>
     
