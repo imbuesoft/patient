b009d11ac068012230e8fdeb9292b39dcbf476c3
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-22 01:12:45 +0530

    Patho Doc [+, -] functionality

--- src/features/PathologyDocument/pages/PathologyForm.jsx
+++ src/features/PathologyDocument/pages/PathologyForm.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Input, Button, Typography } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import { Input, Typography } from 'antd';
+import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import '../assets/patho.css'
 
 const { Title } = Typography;
 
-// const LowHigh = x => (
-//     <span
-//         style={{ color: "#FFA07A", textShadow: "0 0 1em  #ff000099" }}
-//     >{x.children}</span>)
-// const Low = () => (<LowHigh>LOW</LowHigh>)
-// const High = () => (<LowHigh>HIGH</LowHigh>)
-const Low = () => (<span style={{ color: "#e076c9", textShadow: "0 0 1em  #e076c9" }}>LOW</span>)
-const High = () => (<span style={{ color: "#009acc  ", textShadow: "0 0 1em  #009accaa  " }}>HIGH</span>)
+const bold = { fontWeight: "700" }
+
+const Low = () => (<span style={{ color: "#e076c9", cursor: "default", textShadow: "0 0 1em  #e076c9" }}>LOW</span>)
+const High = () => (<span style={{ color: "#009acc", cursor: "default", textShadow: "0 0 1em  #009accaa  " }}>HIGH</span>)
 
 const PathologyForm = ({ pathoData, setPathoData }) => {
-    const bold = { fontWeight: "700" }
+
+    const a = parseFloat;
+
+    const handlePush = (key) => {
+        setPathoData({ ...pathoData, [key]: [...pathoData[key], { name: "", value: "", min: "", max: "", unit: "" }] })
+        console.log("push", key)
+    }
+    React.useEffect(() => {
+        console.log(pathoData);
+    }, [pathoData]);
+
+
+    const handlePop = (key) => {
+        setPathoData({ ...pathoData, [key]: pathoData[key].slice(0, -1) })
+        console.log("pop", pathoData[key])
+    }
+
+    const handleRemove = (key, index) => {
+        // write code that delete the item from the array at give index
+        const temp = [...pathoData[key]];
+        temp.splice(index, 1);
+        setPathoData({ ...pathoData, [key]: temp });
+        console.log(pathoData[key]);
+    }
     return (
         <>
             <div style={{ display: 'grid' }}>
@@ -28,6 +47,7 @@ const PathologyForm = ({ pathoData, setPathoData }) => {
                                 <td></td>
                                 <td><Title level={5} style={{ ...bold, textAlign: "center" }}>Ref. value</Title> </td>
                                 <td><Title level={5} style={{ ...bold }}>Unit</Title> </td>
+                                <td></td>
                             </tr>
                         </thead>
                         <tbody>
@@ -38,30 +58,46 @@ const PathologyForm = ({ pathoData, setPathoData }) => {
                                         <td colSpan={5}>
                                             <h4 style={{ ...bold, margin: " 0.7em 0 0 0", padding: " 0 0.3em", textTransform: "uppercase" }}>
                                                 {key}
-                                                {/* <Button type="primary" icon={<DownloadOutlined />} /> */}
+                                                <span onClick={() => { handlePush(key) }} style={{ display: "inline-grid", height: "1.1rem", width: "1.1rem", margin: "0 0 0 0.5em ", cursor: "pointer", background: "#ddd", borderRadius: "0.3em" }}><PlusOutlined size={"small"} style={{ margin: "auto", fontSize: '0.7em' }} /></span>
+                                                <span onClick={() => { handlePop(key) }} style={{ display: "inline-grid", height: "1.1rem", width: "1.1rem", margin: "0 0 0 0.5em ", cursor: "pointer", background: "#ddd", borderRadius: "0.3em" }}><MinusOutlined size={"small"} style={{ margin: "auto", fontSize: '0.7em' }} /></span>
+
                                             </h4>
                                         </td>
                                     </tr>
                                     {
                                         pathoData[key].map((item, index) => (
-                                            <tr key={index}>
-                                                <td><Input size='small' className='input-border-style'
-                                                    placeholder='name'
-                                                    defaultValue={item.name}
-                                                    style={{ width: "13rem" }}
-                                                /></td>
-                                                <td><Input size='small' className='input-border-style'
-                                                    placeholder='value'
-                                                    defaultValue={item.value}
-                                                    style={{ width: "6em", textAlign: "center" }} /></td>
-                                                <td><div style={{ fontSize: "0.8em", textAlign: "center" }}>{(parseFloat(item.value) < parseFloat(item.min) && <Low />) || (parseFloat(item.value) > parseFloat(item.max) && <High />) || ""}</div></td>
+                                            <tr key={item.name}>
+                                                <td>
+                                                    <Input size='small' className='input-border-style'
+                                                        placeholder='name'
+                                                        defaultValue={item.name}
+                                                        style={{ width: "13rem" }}
+                                                    />
+                                                </td>
+                                                <td>
+                                                    <Input size='small' className='input-border-style'
+                                                        placeholder='value'
+                                                        defaultValue={item.value}
+                                                        style={{ width: "6em", textAlign: "center" }}
+                                                    />
+                                                </td>
+                                                <td>
+                                                    <div style={{ fontSize: "0.8em", textAlign: "center" }}>
+                                                        {(a(item.value) < a(item.min) && <Low />) || (a(item.value) > a(item.max) && <High />) || ""}
+                                                    </div>
+                                                </td>
                                                 <td style={{ display: "flex", }}>
-                                                    <Input size='small' className='input-border-style' defaultValue={item.min} placeholder='min' style={{ width: "4em", textAlign: "right" }} />-
-                                                    <Input size='small' className='input-border-style' defaultValue={item.max} placeholder='max' style={{ width: "4em", textAlign: "left" }} /></td>
-                                                <td><Input size='small' className='input-border-style'
-                                                    placeholder='unit'
-                                                    defaultValue={item.unit}
-                                                    style={{ width: "7em" }} /></td>
+                                                    <Input defaultValue={item.min} placeholder='min' style={{ width: "4em", textAlign: "right" }} size='small' className='input-border-style' />
+                                                    -
+                                                    <Input defaultValue={item.max} placeholder='max' style={{ width: "4em", textAlign: "left" }} size='small' className='input-border-style' /></td>
+                                                <td>
+                                                    <Input size='small' className='input-border-style'
+                                                        placeholder='unit'
+                                                        defaultValue={item.unit}
+                                                        style={{ width: "7em" }} />
+                                                    <span onClick={() => { handleRemove(key, index) }} style={{ display: "inline-grid", height: "1.1rem", width: "1.1rem", margin: "0 0 0 0.5em ", cursor: "pointer", background: "#ddd", borderRadius: "0.3em" }}><MinusOutlined size={"small"} style={{ margin: "auto", fontSize: '0.7em' }} /></span>
+
+                                                </td>
                                             </tr>
                                         ))
                                     }
