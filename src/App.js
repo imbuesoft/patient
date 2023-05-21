40419651d85e7b73c90cb06cbd2fef00a1c747de
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-22 01:13:04 +0530

    Added PathologyDocument

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
+import { useState } from 'react';
+import { Typography } from 'antd';
+
+// DO NOT REMOVE THESE IMPORTS 
+
 import BmiCalculator from './features/BmiCalculator/';
 import BmiCalculatorBootstrap from './features/BmiCalculatorBootstrap';
 import Search from './features/Search/';
+import PathologyDocument from './features/PathologyDocument';
+
 import fruitsAndVegetables from './api/tempData.json'
-import { Typography } from 'antd';
+import PathoData from './api/PathoData.json';
 
 const { Title } = Typography;
 
 function App() {
+
+  const [pathoData, setPathoData] = useState(PathoData);
+
   return (
     <>
       {/***** 🧮 BMI calculator bootstrap *****/}
       {/* <BmiCalculatorBootstrap /> */}
-      
+
       {/***** 🧮 BMI calculator ant-design *****/}
       {/* <BmiCalculator /> */}
 
       {/***** 🔍 Search box *****/}
-      <div className="container " style={{ height: "100vh" }}>
-        <Title className='mt-4'>Search fruits and vegetables by name, color or category</Title>
-        <div className="d-flex flex-column justify-content-center mt-5">
-          <Search data={fruitsAndVegetables} />
-        </div>
-      </div>
+      {/* <div style={{ height: "100vh", width: "100vw", display: "grid" }}><div style={{ height: "100vh", maxWidth: "60em", margin:"auto" }}><Title style={{margin:"1em 0"}}>Search fruits and vegetables by name, color or category</Title><div className="d-flex flex-column justify-content-center mt-5"><Search data={fruitsAndVegetables} /></div></div></div> */}
+
+      {/***** 🧬 Pathology Document *****/}
+      <PathologyDocument pathoData={pathoData} setPathoData={setPathoData} />
+
     </>
   );
 }
