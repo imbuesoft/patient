83783d21afa1e68ab0d40b1bb6018e8d59cfa88a
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-22 22:46:45 +0530

    added bootstrap cdn to bmi bootstrap

--- src/features/BmiCalculatorBootstrap/index.jsx
+++ src/features/BmiCalculatorBootstrap/index.jsx
@@ -4,6 +4,7 @@ const index = () => {
     return (
         <>
         <div style={{height:"100vh", width:"100vw"}}>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
             <BMIForm />
         </div>
         </>
