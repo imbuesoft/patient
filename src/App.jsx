a988382e3b99db2907e20f47fec5287eb4516006
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-23 10:45:38 +0530

    Seperate SpeechToText in NewHook.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import useSpeechToText from './NewHook';
 
 const SpeechToText = () => {
-  const [text, setText] = useState('');
-  const [language, setLanguage] = useState('en-IN');
-  const [recognitionActive, setRecognitionActive] = useState(false);
-  let recognition = null;
-
-  const requestMicrophonePermission = () => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then(() => {
-        console.log('Microphone permission granted.');
-      })
-      .catch((error) => {
-        console.error('Error requesting microphone permission:', error);
-      });
-  };
-
-  const startRecognition = () => {
-    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = language;
-    recognition.interimResults = true;
-    recognition.continuous = true; // Set continuous to true
-
-    recognition.onstart = () => {
-      setRecognitionActive(true);
-    };
-    
-    recognition.onresult = (event) => {
-      const lastResultIndex = event.results.length - 1;
-      const interimTranscript = event.results[lastResultIndex][0].transcript;
-      const isFinal = event.results[lastResultIndex].isFinal;
-
-      setText((prevText) => {
-        let newText = prevText;
-
-        if (isFinal) {
-          newText += interimTranscript + ' ';
-        }
-
-        return newText;
-      });
-    };
-
-
-    recognition.onerror = (event) => {
-      console.error('Speech recognition error:', event.error);
-    };
-
-    recognition.start();
-  };
-
-  const stopRecognition = () => {
-    if (recognitionActive) {
-      setRecognitionActive(false);
-      recognition.stop();
-    }
-  };
-
-  const handleLanguageChange = (event) => {
-    setLanguage(event.target.value);
-  };
-
-  useEffect(() => {
-    if (recognitionActive) {
-      startRecognition();
-    }
-    return () => {
-      if (recognitionActive) {
-        stopRecognition();
-      }
-    };
-  }, [recognitionActive]);
+  const {
+    text,
+    language,
+    recognitionActive,
+    requestMicrophonePermission,
+    startRecognition,
+    stopRecognition,
+    handleLanguageChange,
+  } = useSpeechToText();
 
   return (
     <div>
@@ -81,7 +20,7 @@ const SpeechToText = () => {
         <option value="hi-IN">Hindi (India)</option>
         <option value="gu-IN">Gujarati (India)</option>
       </select>
-      <button onClick={() => setRecognitionActive(true)} disabled={recognitionActive}>
+      <button onClick={startRecognition} disabled={recognitionActive}>
         Start
       </button>
       <button onClick={stopRecognition} disabled={!recognitionActive}>
