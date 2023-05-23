2cf3d486884d54775640be950b7d248616d37ab6
mirajsankdecha <mirajsankdecha@gmail.com>
2023-05-23 11:48:37 +0530

    i create a patient log table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Form, Input, Radio, Card } from "antd";
+import "./App.css";
 
-function App() {
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+
+const plainOptions = ["Male", "Female", "Other"];
+
+const App = () => {
+  const [data, setData] = useState({ name: "", age: "", gender: "" });
+  const [info, setInfo] = useState([]);
+
+  const inputEvent = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+  console.log(data);
+
+  const onFinish = () => {
+    setInfo([...info, data]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <Card
+          title={
+            <h2>
+              <center>Patient-log</center>
+            </h2>
+          }
+          bordered={false}
+          style={{
+            width: "100vh",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Form
+            {...layout}
+            name="nest-messages"
+            onFinish={onFinish}
+            style={{
+              maxWidth: 600,
+            }}
+          >
+            <Form.Item label="Name">
+              <Input name="name" onChange={inputEvent} value={data.name} />
+            </Form.Item>
+            <Form.Item label="Age">
+              <Input name="age" onChange={inputEvent} value={data.age} />
+            </Form.Item>
+
+            <Form.Item label="Gender">
+              <Radio.Group
+                name="gender"
+                options={plainOptions}
+                onChange={inputEvent}
+                value={data.gender}
+              />
+            </Form.Item>
+            <Form.Item
+              wrapperCol={{
+                ...layout.wrapperCol,
+                offset: 8,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody>
+              {info.map((prevalues, index) => {
+                const { name, age, gender } = prevalues;
+                return (
+                  <tr key={index}>
+                    <td>{name}</td>
+                    <td>{age}</td>
+                    <td>{gender}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Card>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
