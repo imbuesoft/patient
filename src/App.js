9bc69260980ef3f4a2892fd732a22aa4aaed7bc1
dhararamavat11 <133778240+dhararamavat11@users.noreply.github.com>
2023-05-30 17:53:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const App = () => {
+    return  (
+     <div class="card" style={{ width: "300px", height: "120px" }}>
+                   <div class="card-body">
+                           <div class="d-flex h-100 w-100">
+                               <div class="d-grid">
+                                   <img style={{margin:"auto"}} height={75} width={75} src="https://i.pinimg.com/originals/a5/56/3d/a5563d5eaa1e9076ddddad22496df333.png" alt="Logo" />
+                               </div>
+                               <div class="d-flex flex-column justify-content-center ps-3" >
+                                   <strong>30 Lakh +</strong>
+                                   Downloads
+                               </div>
+                       </div>
+                   </div>
+               </div>
+   )
+   };
+   export default App;
