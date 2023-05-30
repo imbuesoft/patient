89b66cf277121cac11d4c3ff93c614c848284517
KhushiMenpara <khushimenpara123@gmail.com>
2023-05-30 15:53:40 +0530

    doccard

---
+++ card/src/components/Avatar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Avatar(props) {
+  return <img className="rect-img" src={props.image} alt="avatar_img" />;
+}
+
+export default Avatar;
---
+++ card/src/contacts.js
@@ -0,0 +1,14 @@
+const contacts = [
+  {
+    id: "1",
+    name: "General Physicians",
+    des:"for common medical checkups",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQU8TWi1YmUlFHWT6rdgqzFKqlv-Hkcs-FlYMCHjZH770g92JczizbfUVZjnCBb_3KFTs&usqp=CAU",
+    price: <h2>Rs 399 Onwards</h2>,
+    
+  },
+  // Add more contact objects if needed
+];
+
+export default contacts;
