76270abbea97b899d380532c9cd700662d073b09
unknown <imbuesoftworld@gmail.com>
2023-05-30 19:14:08 +0530

    meet work services done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Slider from './Component/Slider/Slider';
+import Service from './Component/Service/Service';
 
 function App() {
   return (
     <div className="">
       <Navbar />
       <Slider />
+
+      <div className="container mt-5">
+        <div className='row'>
+          <Service />
+          <Service />
+          <Service />
+          <Service />
+          <Service />
+          <Service />
+        </div>
+      </div>
 
     </div>
   );
