d4dd37d4d377fe497e49adde9b9836bce3249959
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-05-31 01:18:46 +0530

    Capsule Responsive

--- src/Component/Capsule/Capsule.jsx
+++ src/Component/Capsule/Capsule.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Capsule() {
     return (
-        <div className='col-3'>
-            <div className="card" style={{ width: "300px", height: "120px" }}>
-                <div className="card-body">
-                    <div className="d-flex h-100 w-100">
-                        <div className="d-grid">
+        <div className='col-6 col-md-4 col-lg-3 col-xxl-3 py-2'>
+            <div className="card m-auto" style={{ width: "auto", height: "auto", maxWidth:"15em", borderRadius: "1em" }}>
+                <div className="card-body p-0">
+                    <div className="d-flex flex-wrap flex-sm-nowrap h-100 w-100">
+                        <div className="d-grid m-3 w-100">
                             <img style={{ margin: "auto" }} height={75} width={75} src="https://i.pinimg.com/originals/a5/56/3d/a5563d5eaa1e9076ddddad22496df333.png" alt="Logo" />
                         </div>
-                        <div className="d-flex flex-column justify-content-center ps-3" >
-                            <strong>30 Lakh +</strong>
+                        <div className="d-flex my-1 w-100 text-center  text-sm-start flex-column justify-content-center" >
+                            <strong>50 Lakh +</strong>
                             Downloads
                         </div>
                     </div>
