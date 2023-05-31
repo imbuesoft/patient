849ea36e8261e83aed1549a375560ac65a7558d0
Jay Sanghani <133778202+jaysanghani3@users.noreply.github.com>
2023-05-31 13:26:09 +0530

    Create a Searchbar compnent for City and Specialition of Doctor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-// import Body from './Body';
-// import Navbar from './Navbar';
 import Searchbar from './search_bar/Searchbar';
 
 function App() {
   return (
     <div className="App">
-      {/* "<Navbar/>
-      <Body/>" */}
       <Searchbar/>
     </div>
   );
--- src/search_bar/Searchbar.js
+++ src/search_bar/Searchbar.js
@@ -1,83 +1,16 @@
-import React, { useState } from "react";
-import cityData from "./cityList.json";
+import React from "react";
 import "./Searchbar.css";
+import CitySearch from "./CitySearch";
+import SpecialitiesSearch from "./SpecialitiesSearch";
 
 const Searchbar = () => {
-  const [click, setClick] = useState(false);
-  const [input, setInput] = useState("");
-
-  const searchItem = cityData.filter((i) => {
-    if (input == "") {
-      return i;
-    } else if (i.state.toLowerCase().includes(input.toLowerCase())) {
-      return i;
-    } else {
-      // i.city.forEach((item) => {
-      //   if (item.toLowerCase().includes(input.toLowerCase())) return item;
-      // });
-    }
-  });
-
-  console.log(searchItem);
-
-  const handleClick = () => {
-    setClick(true);
-  };
-
   return (
-    <div
-      className="mt-5"
-      style={{
-        backgroundImage: `url("https://www.practostatic.com/web-assets/home/assets/images/homepage.06859593240c3efd483fe48951cfe6ff.svg")`,
-        height: "600px",
-      }}
-    >
-      <div
-        className="fw-bold text-center text-white pt-5"
-        style={{ fontSize: "50px" }}
-      >
-        Your home for health
-      </div>
+    <div className="mt-5" style={{ backgroundImage: `url("https://www.practostatic.com/web-assets/home/assets/images/homepage.06859593240c3efd483fe48951cfe6ff.svg")`,height: "600px"}}>
+      <div className="fw-bold text-center text-white pt-5"style={{ fontSize: "50px" }}>Your home for health</div>
       <div className="fs-2 text-center text-white pt-5">Find and Book</div>
-
-      <div
-        className="d-inline-flex mt-3"
-        style={{ width: "900px", height: "60px", display: "flex" }}
-      >
-        <div className="ct input-group bg-white border-end w-75">
-          <i className="bi bi-geo-alt fs-5 p-2 mx-2 my-auto"></i>
-          <input
-            className="ctSearch border-0 fs-5"
-            value={input}
-            placeholder="Search your Location ..."
-            onChange={(e) => setInput(e.target.value)}
-            onClick={handleClick}
-          />
-          {/* <input className="ctSearch border-0 fs-5" type="text" placeholder="City"/> */}
-          <div className="mt-4 suggestionBox overflow-auto">
-            {click
-              ? searchItem.map((i) => {
-                  return (
-                    <div className="border  loc">
-                      {/* <div className='fs-4 text-start'>{i.city}</div> */}
-                      <div className="stateNm py-2 fs-5 text-start ms-3">
-                        {i.state}
-                      </div>
-                    </div>
-                  );
-                })
-              : ""}
-          </div>
-        </div>
-
-        <div className="input-group bg-white">
-          <i className="bi bi-search fs-5 p-2 mx-2 my-auto"></i>
-          <input
-            className="drSearch border-0 fs-5"
-            type="text"
-            placeholder="Search doctor, clinics, hospitals, etc."
-          />
-        </div>
+      <div className="d-inline-flex mt-3" style={{ width: "900px", height: "60px", display: "flex" }}>
+        <CitySearch/>
+        <SpecialitiesSearch/>
       </div>
     </div>
   );
