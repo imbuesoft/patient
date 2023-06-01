504e7261bfdd7a7790a684cee17752d04aea2ba4
KhushiMenpara <khushimenpara123@gmail.com>
2023-06-01 16:36:49 +0530

    healthcheckuppackage

--- card/src/components/Avatar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Avatar(props) {
-  return <img className="rect-img" src={props.image} alt="avatar_img" />;
-}
-
-export default Avatar;
--- src/App.js
+++ src/App.js
@@ -1,57 +1,154 @@
-import React from "react";
-import QRCode from 'qrcode.react';
-import { Divider } from 'antd';
-import Header from './Header';
-import H1 from './H1';
-import H2 from './H2';
+import React from 'react';
+import p1 from './p1.png';
+const HealthCheckupPackage = () => {
+  const cardStyle = {
+    width: "27rem",
+    height: "21rem",
+    borderRadius: "20px",
+  };
 
-const App=()=>{
-  
-  const person={
-    name:"Yash M. Patel",
-    age:21 ,
-    sex:"Male",
-    pid:"555"
+  const titleStyle = {
+    color: "#02475B",
+  };
+
+  const subtitleStyle = {
+    color: "#02475B",
+  };
+
+  const descriptionStyle = {
+    color: "#02475B",
+  };
+
+  const mrpStyle = {
+    color: "#02475B",
+    textDecoration: "line-through",
+  };
+
+  const offerStyle = {
+    color: "#02475B",
+  };
+
+  const moreStyle = {
+    color: "#FC9916",
+    fontWeight: 600,
+    paddingLeft: "5px",
+    verticalAlign: "top",
+    boxSizing: "inherit",
+  };
+
+  const buttonStyle = {
+    fontSize: "15px",
+    padding: "4px 8px",
+    borderRadius: "4px",
+  };
+
+  const addToCartStyle = {
+    position: "absolute",
+    bottom: "10px",
+    right: "10px",
+    backgroundColor: "transparent",
+    color: "#FC9916",
+    padding: "8px 16px",
+    borderRadius: "7px",
+    fontSize: "18px",
+    fontWeight: "bold",
+    border: "2px",
   };
   
-  const h1 ={
-    name :"Sample Collected At:",
-    address:"125,Shivam Bungalow,S GRoad,Ahmadabad",
-    refby:"Dr. Hiren Shah."
+
+  const bestValueStyle = {
+    position: "absolute",
+    top: "3px",
+    right: "2px",
+    backgroundColor: "#1889a7",
+    color: "white",
+    padding: "4px 8px",
+    borderRadius: "7px",
+    fontSize: "14px",
+    fontWeight: "bold",
+    display: "flex",
+    alignItems: "center",
   };
-  const h2 ={
-    registered: "02:31 PM 02 Dec,21",
-    collected: "03:11 PM 02 Dec,21",
-    reported: "04:35 PM 02 Dec,21"
-    
+
+  const starStyle = {
+    color: "#f2b633",
+    marginRight: "5px",
   };
-  return(
-    <div style={{display:'flex',justifyContent:'center'}}>
-      <div style={{margin:'0 200px 0 40px', borderLeftWidth: 1,borderLeftColor: 'gray'}}>
-        
-      <Header
-        {...person}>
-      </Header>
-      
-    </div><QRCode Value="http://www.vistaderm.in"/> < Divider
-   type="vertical" style={{ height: "200px", backgroundColor: "#ede9c0" }}
-/>
-
-    <div style={{margin: '0 200px 0 40px'}}>
-      <H1 {...h1}/>
-
-      
-
+  const cashbackStyle = {
+    position: "absolute",
+    bottom: "10px",
+    left: "10px",
+    backgroundColor: "#fafbfc",
+    color: "#0087BA",
+    padding: "8px 16px",
+    borderRadius: "7px",
+    fontSize: "14px",
+    fontWeight: "bold",
+  };
+  const cashbackLogoStyle = {
+    marginRight: "5px",
+    height:"30px",
+    width:"40px",
+  };
+  
+  return (
+    <div className="card" style={cardStyle}>
+    <div className="top">
+      <div className="card-body">
+        <button className="btn btn-primary" style={bestValueStyle}>
+          <span style={starStyle}>★</span> Best Value
+        </button>
+        <h5 className="card-title" style={titleStyle}>
+          Apollo Full Body Checkup - <br />
+          Essential
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted" style={subtitleStyle}>
+          TOTAL TESTS: 77
+        </h6>
+        <h6 className="card-subtitle mb-2 text-muted" style={subtitleStyle}>
+          GLUCOSE, FASTING
+        </h6>
+        <h6 className="card-subtitle mb-2 text-muted" style={descriptionStyle}>
+          Erythrocyte Sedimentation Rate (Esr)
+          <span style={moreStyle}>+75 More</span>
+        </h6>
+      </div>
+      <hr/>
+      <div className="bottom">
+        <div className="card-body">
+          <div className="row">
+            <div className="col">
+              <div>
+             <h6 className="card-subtitle mb-2 text-muted" style={mrpStyle}>
+              MRP ₹9639 <button className="btn btn-success rounded-pill" style={buttonStyle}>
+                    55% Off
+                    </button>
+                    </h6>
+                  <h6 className="card-subtitle mb-2 text-muted" style={offerStyle}>
+                  <b>MRP ₹4299</b>
+                </h6>
+              </div>
+            </div>
+            <div className="col">
+              <button className="btn btn" style={addToCartStyle}>
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button className="btn btn" style={cashbackStyle}>
+              <img src={p1} alt="p1" style={cashbackLogoStyle} />
+                Extra ₹450 Cashback
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    < Divider
-   type="vertical" style={{ height: "200px", backgroundColor: "#ede9c0" }}
-/>
-    <div style={{margin: '0 200px 0 40px'}}>
-      <H2 {...h2}/>
-    </div>
-   
-    </div>
+  </div>
+  
   );
-
 };
-export default App;
+
+export default HealthCheckupPackage;
