a3f714057e1a4ad4f1b50d0bac35d4c3e2c85feb
unknown <dhararamavat11@gmail.com>
2023-06-01 12:28:03 +0530

    smallcard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
+import SmallCard from './Components/SmallCard/App'
+
+
 const App = () => {
-    return  (
-     <div class="card" style={{ width: "300px", height: "120px" }}>
-                   <div class="card-body">
-                           <div class="d-flex h-100 w-100">
-                               <div class="d-grid">
-                                   <img style={{margin:"auto"}} height={75} width={75} src="https://i.pinimg.com/originals/a5/56/3d/a5563d5eaa1e9076ddddad22496df333.png" alt="Logo" />
-                               </div>
-                               <div class="d-flex flex-column justify-content-center ps-3" >
-                                   <strong>30 Lakh +</strong>
-                                   Downloads
-                               </div>
-                       </div>
-                   </div>
-               </div>
-   )
-   };
-   export default App;
+    return (
+        <>
+            <SmallCard />
+        </>
+    )
+};
+export default App;
