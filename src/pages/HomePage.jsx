19ba5a0421485bd7019b2e9109dc79ccaa135463
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-06-02 17:13:47 +0530

    Hompage fix spacing and containers

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,19 +9,24 @@ import Searchbar from '../Component/Searchbar/Searchbar';
 import VideoConsult from '../Component/VideoConsult';
 
 const Container = (x) =>
-  <div className={"container-fluid mt-5 " + x.className} style={{ maxWidth: "125em", ...x.style }}>
+  <div className={"container-fluid " + (x.className || "")} style={{ maxWidth: "125em", ...x.style }}>
     {x.children}
   </div>
+
+const Space = ({ h }) => <div style={{ height: h ?? "2em" }}></div>
 
 function App() {
   return (
     <div className="App">
 
-      <div style={{ backgroundColor: '#429ca533' }}>
+      <div style={{ backgroundColor: 'var(--color-secondary)' }}>
         <Container>
           <Navbar />
         </Container>
       </div>
+
+      <Space />
+
 
       {/* <Container>
         <Searchbar />
@@ -31,44 +36,56 @@ function App() {
         <Slider />
       </Container>
 
-      <div className="container mt-5">
-        <div className='row m-0 p-0 '>
-          <Service />
-          <Service />
-          <Service />
-          <Service />
-          <Service />
-          <Service />
-        </div>
-      </div>
+      <Space />
 
-      <div className="container mt-5">
-        <div className='row'>
-          <Capsule />
-          <Capsule />
-          <Capsule />
-          <Capsule />
+      <Container>
+        <div className='row gap-0 gap-lg-0 gap-xl-3 justify-content-center m-0 p-0 '>
+          <Service />
+          <Service />
+          <Service />
+          <Service />
+          <Service />
+          <Service />
         </div>
-      </div>
+      </Container>
 
-      <div className="container mt-5">
-        <div className='row'>
+      <Space />
+
+      <Container>
+        <div className='row gap-3 gap-sm-0 gap-lg-4 gap-xl-5 justify-content-center'>
+          <Capsule />
+          <Capsule />
+          <Capsule />
+          <Capsule />
+        </div>
+      </Container>
+
+      <Space />
+
+      <Container>
+        <div className='row flex-xxl-row  justify-content-center'>
           <Features />
           <Features />
           <Features />
           <Features />
         </div>
-      </div>
+      </Container>
+
+      <Space />
 
       <Container>
         <Testimonial />
       </Container>
 
+      <Space />
+
       <Container>
         <VideoConsult />
       </Container>
 
-      <div className='mt-5' style={{background:"#232477"}}>
+      <Space />
+
+      <div className='mt-5' style={{ background: "#232477" }}>
         <Container>
           <Footer />
         </Container>
